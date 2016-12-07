b9bdd2c9a339ebf41b448c3af9f5bfffd703a6b3
youwei91 <youwei.teh@jobstreet.com>
2016-12-07 15:17:47 +1100

    Updtae get all interview list

--- service/functions/getAllInterviewList.js
+++ service/functions/getAllInterviewList.js
@@ -1,23 +1,15 @@
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-module.exports.getAllInterviewList = (event, context, callback) => {
-  var docClient = new AWS.DynamoDB.DocumentClient();
-  docClient.scan({TableName: "Music"}, function(err, data) {
-    if (err)
-      console.log(JSON.stringify(err, null, 2));
-    else
-      console.log(JSON.stringify(data, null, 2));
-  });
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'You have reached the interview scheduler backend.',
-  //     input: event,
-  //   }),
-  // };
-  //
-  // callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports = () => {
+  return {
+    getAllInterviewList: ()=> {
+      docClient.scan({TableName: "interview-scheduler-interview-data"}, (err, data) => {
+        if (err)
+          console.log(JSON.stringify(err, null, 2));
+        else
+          return JSON.stringify(data);
+      });
+    }
+  }
 };
