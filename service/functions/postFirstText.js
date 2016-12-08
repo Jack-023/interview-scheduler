9c5e5aa19f6cf6052c796868e2863d60f3aadb53
youwei91 <youwei.teh@jobstreet.com>
2016-12-08 15:06:04 +1100

    Update SMS service

--- service/functions/postFirstText.js
+++ service/functions/postFirstText.js
@@ -1,41 +1,32 @@
-const AWS = require("aws-sdk");
-const docClient = new AWS.DynamoDB.DocumentClient();
+'use strict';
+const aws = require("aws-sdk");
+const docClient = new aws.DynamoDB.DocumentClient();
 const twilio = require("../lib/twilioService");
 
-module.exports = () => {
-    return {
-        getAllInterviewList: (id)=> {
-            var param = {
-                TableName: `interview-scheduler-interview-data`,
-                ConsistentRead: true,
-                Key: {
-                    interviewId: id
-                }
-            };
-
-            docClient.get(param, (err, data) => {
-                if (err) {
-                    console.log(JSON.stringify(err, null, 2));
-                    reject(err);
-                }
-                else {
-                    const ts = twilio();
-                    ts.sendMessage(data);
-                    resolve();
-                }
-            });
-        }
-    };
-    // const response = {
-    //   statusCode: 200,
-    //   body: JSON.stringify({
-    //     message: 'You have reached the interview scheduler backend.',
-    //     input: event,
-    //   }),
-    // };
-    //
-    // callback(null, response);
-
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.postFirstText = (data, context, callback)=> {
+    const payload = JSON.parse(data.body);
+    if (!payload.id) {
+        callback(null, {
+            statusCode: 500
+        });
+    } else {
+        var param = {
+            TableName: `interview-scheduler-interview-data`,
+            ConsistentRead: true,
+            Key: {
+                interviewId: payload.id
+            }
+        };
+        docClient.get(param, (err, data) => {
+            if (err) {
+                console.log(JSON.stringify(err, null, 2));
+                reject(err);
+            }
+            else {
+                const ts = twilio();
+                ts.sendMessage(data);
+                resolve();
+            }
+        });
+    }
 };
