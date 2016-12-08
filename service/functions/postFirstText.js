89cdeb44bd1c96b31fc4d083cc40dbc629505cab
Jack Jordan <jjordan@seek.com.au>
2016-12-08 17:04:46 +1100

    Integration stuff

--- service/functions/postFirstText.js
+++ service/functions/postFirstText.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const aws = require("aws-sdk"); //eslint-disable-line
+const aws = require('aws-sdk'); //eslint-disable-line
+const moment = require('moment');
 
 const docClient = new aws.DynamoDB.DocumentClient();
 const twilio = require('../lib/twilioService');
@@ -21,8 +22,15 @@ module.exports.postFirstText = (id) => {
             }
             else { // eslint-disable-line
                 const ts = twilio();
-                ts.sendMessage(data).then((message) => {
-                    console.log(message);
+                const date = moment(data.item.interviewTime).tz('AEDT').format('dddd, MMMM Do YYYY');
+                const time = moment(data.item.interviewTime).tz('AEDT').format('hh:mm');
+                console.log(data);
+                const message = `${data.item.candidateName}, you have an interview with ${data.item.advertiserName} from ${data.item.advertiserCompany}`
+                + ` on ${date} at ${time}. Can you confirm that still works for you? A YES or NO will do! Please ring ${data.item.advertiserPhNo} if`
+                + ' you cannot attend or want to reschedule your interview.';
+
+                ts.sendMessage(data.item.candidatePhNo, message).then((resp) => {
+                    console.log(resp);
                     resolve();
                 }).catch((error) => {
                     console.log(error);
--- service/lib/twilioService.js
+++ service/lib/twilioService.js
@@ -6,21 +6,21 @@ const twilio = require('twilio'); // eslint-disable-line
 
 module.exports = () => {
     return {
-        sendMessage: (data) => {
-          console.log(data);
-          console.log(config);
-            if (data.Item) {
+        sendMessage: (to, message) => {
+            console.log(config);
+            if (to && message) {
                 const twilioClient = new twilio.RestClient(config.twilio.accountSid, config.twilio.authToken);
-                var message = "TEST MESSAGE";
                 return new Promise((res, rej) => twilioClient.messages.create({
                     body: message,
-                    to: "+61" + /\d+/.exec(data.Item.candidatePhNo)[0].substring(1),
+                    to: "+61" + /\d+/.exec(to)[0].substring(1),
                     from: "+61 439 249 641"
-                }, function (err, message) {
-                    if (err)
+                }, (err, resp) => {
+                    if (err) {
                         rej(err);
-                    else
-                        res(message);
+                    }
+                    else {
+                        res(resp);
+                    }
                 }));
             }
         }
